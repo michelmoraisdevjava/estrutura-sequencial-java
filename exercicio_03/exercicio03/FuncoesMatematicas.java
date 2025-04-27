2a4062640a0f347ae95df8c3a0b68e687f801b2f
Michel Morais <valentimmichel@gmail.com>
2025-04-27 18:22:55 -0300

    Gerar dois numeros aleatorios e informar o maior deles

--- exercicio_03/exercicio03/FuncoesMatematicas.java
+++ exercicio_03/exercicio03/FuncoesMatematicas.java
@@ -1,18 +1,13 @@
 package exercicio03;
 
-import java.util.Scanner;
-
 public class FuncoesMatematicas {
 
-	public static void main(String[] args) { //Calcular o quadrado de um número qualquer
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) { //Gerar dois números aleatórios de 1 a 100 e informar qual é o maior.
+		int resultadoAleatorio1 = (int)(Math.random() * 100) + 1;
+		int resultadoAleatorio2 = (int)(Math.random() * 100) + 1;
 		
-		System.out.print("Informe um número qualquer: ");
-		double numero = sc.nextDouble();
-		
-		System.out.printf("O quadrado do número é: %.2f", Math.pow(numero, 2.0));
-		
-		sc.close();
+		System.out.printf("Primeiro número aleatório: %d%n", resultadoAleatorio1);
+		System.out.printf("Segundo número aleatório: %d%n", resultadoAleatorio2);
+		System.out.printf("O maior deles é: %d%n", Math.max(resultadoAleatorio1, resultadoAleatorio2));
 	}
-
 }
